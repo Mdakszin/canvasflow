b71e26a03813a750d1543ca2de19f3b97fd27b00
Mdakszin <mdakszin@gmail.com>
2025-09-22 13:03:29 +0200

    "Fix: Tailwind miscinfiguration"

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,14 +2,18 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   darkMode: "class",
-  content: [ /* ... */ ],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}', // <-- This is CRUCIAL for the App Router
+  ],
   theme: {
     extend: {
       colors: {
         text: 'var(--color-text)',
         background: 'var(--color-background)',
-        primary: 'var(--color-primary)',   // Headings
-        secondary: 'var(--color-secondary)', // Links & Icons
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
         accent: 'var(--color-accent)',
       }
     },
